a1dfa5b0f805adb3b8703be144d673b910051fdb
Evgenii <markelovea1990@yandex.ru>
2022-03-19 16:35:49 +0300

    Lesson 1 Practice

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.app1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_digit_comparer);
+
+        EditText inputOne = findViewById(R.id.input_one);
+        EditText inputTwo = findViewById(R.id.input_two);
+        TextView Textresult = findViewById(R.id.text_result);
+        Button btn_compare = findViewById(R.id.btn_compare);
+        btn_compare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputOnevalue = inputOne.getText().toString();
+                String inputTwovalue = inputTwo.getText().toString();
+                try {
+                    Integer intOne = Integer.valueOf(inputOnevalue);
+                    Integer intTwo = Integer.valueOf(inputTwovalue);
+
+                    if (intOne.equals(intTwo)) {
+                        Textresult.setText("Равно");
+                    } else {
+                        Textresult.setText("Не равно");
+                    }
+                } catch (NumberFormatException exeption) {
+                    Textresult.setText("Ввводи только числа");
+                }
+
+            }
+
+        });
     }
 }
